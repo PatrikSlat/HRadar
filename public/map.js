7d0d8115372b1092c57a3b4c61e4bd034cbf117f
PatrikSlat <patrik.slat@gmail.com>
2024-05-26 15:29:31 +0200

    Added page loader for inital content load

--- public/map.js
+++ public/map.js
@@ -1,3 +1,13 @@
+//Page loader
+document.addEventListener("DOMContentLoaded", ()=> {
+    const preloader = document.getElementById('preloader');
+
+    setTimeout(() =>{
+        preloader.style.display = 'none';
+    }, 1500)
+})
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const map = L.map('map').setView([45.1, 16.5], 8);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
